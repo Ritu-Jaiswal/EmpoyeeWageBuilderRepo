ac87b696fe6e62188c5e8b1367b8631421265ee6
Ritu-Jaiswal <jairitu2003@gmail.com>
2022-03-11 22:28:21 +0530

    MODIFIED: Employee Daily Wage Calculation

--- src/com/infogalaxy/EmpoyeeWageBuilder.java
+++ src/com/infogalaxy/EmpoyeeWageBuilder.java
@@ -4,17 +4,22 @@ import java.util.Random;
 
 public class EmpoyeeWageBuilder {
     public static void main(String[] args){
-    //pass - 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
-    //UC - 1 Check Attendance
+        // UC 2 - Daily Wage Calculation
+        // pass - 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
+        final int WAGE_PER_HR = 20;
+        final int  FULL_DAY_HR = 8;
+        int total_wage = 0;
     Random random = new Random();
     int checkAttendance = random.nextInt()%2;
-    System.out.println(checkAttendance);
+        // UC 1 - Check Attendance
 
     if (checkAttendance == 0){
         System.out.println("Employee is Absent.");
     }
     else{
             System.out.println("Employee is Present.");
+            total_wage = FULL_DAY_HR  * WAGE_PER_HR;
         }
+        System.out.println("Total Wage = "+total_wage);
     }
 }
