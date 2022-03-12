b6706d28f9e5c865ee5e4d9753b184fd20a7a246
Ritu-Jaiswal <jairitu2003@gmail.com>
2022-03-12 23:30:55 +0530

    MODIFIED: Check Employee Type

--- src/com/infogalaxy/EmpoyeeWageBuilder.java
+++ src/com/infogalaxy/EmpoyeeWageBuilder.java
@@ -8,17 +8,26 @@ public class EmpoyeeWageBuilder {
         // pass - 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
         final int WAGE_PER_HR = 20;
         final int  FULL_DAY_HR = 8;
+        final int  PART_TIME_HR = 4;
         int total_wage = 0;
     Random random = new Random();
     int checkAttendance = random.nextInt()%2;
         // UC 1 - Check Attendance
-
+        // UC 3 - Part time/Full time Wage Calculation
     if (checkAttendance == 0){
         System.out.println("Employee is Absent.");
     }
     else{
             System.out.println("Employee is Present.");
-            total_wage = FULL_DAY_HR  * WAGE_PER_HR;
+            int empType = random.nextInt()%2;
+            if(empType == 0){
+                System.out.println("Employee is Part Time.");
+                total_wage = WAGE_PER_HR * PART_TIME_HR;
+            }
+            else {
+                System.out.println("Employee is Full Time.");
+                total_wage = FULL_DAY_HR * WAGE_PER_HR;
+            }
         }
         System.out.println("Total Wage = "+total_wage);
     }
