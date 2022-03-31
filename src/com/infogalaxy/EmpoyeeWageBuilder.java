dd1986d6cbe91c48b266d67e85d19b9a8ae2a4f7
Ritu-Jaiswal <jairitu2003@gmail.com>
2022-03-31 15:35:58 +0530

    MODIFIED: Total Working Hours

--- src/com/infogalaxy/EmpoyeeWageBuilder.java
+++ src/com/infogalaxy/EmpoyeeWageBuilder.java
@@ -4,42 +4,41 @@ import java.util.Random;
 
 public class EmpoyeeWageBuilder {
     public static void main(String[] args) {
-        // UC 2 - Daily Wage Calculation
-        // pass - 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
-        final int WAGE_PER_HR = 20;
-        final int FULL_DAY_HR = 8;
-        final int PART_TIME_HR = 4;
+        int working_days = 0;
+        int total_hrs = 0;
 
         Random random = new Random();
+        while(working_days < Constants.TOTAL_WORKING_DAYS && total_hrs <= Constants.TOTAL_WORKING_HRS) {
+            working_days++;
+        int total_wage = 0;
+        System.out.println("Day : " +working_days);
+        int checkAttendance = random.nextInt() % 2;
 
-        for (int i=0 ; i <= 20; i++) {
-            int total_wage = 0;
-            System.out.println("Day :" + i);
-            int checkAttendance = random.nextInt() % 2;
-            // UC 1 - Check Attendance
-            // UC 3 - Part time/Full time Wage Calculation
-            // UC 4 - convert into SwichCase Code
-            // UC 5 - Wage for Month
-            if (checkAttendance == 0) {
-                System.out.println("Employee is Absent.");
-            } else {
-                System.out.println("Employee is Present.");
-                int empType = random.nextInt() % 2;
-                switch (empType) {
-                    case 0:
-
-                        System.out.println("Employee is Part Time.");
-                        total_wage = WAGE_PER_HR * PART_TIME_HR;
-                        break;
-                    case 1:
-                    case -1:
-                        System.out.println("Employee is Full Time.");
-                        total_wage = FULL_DAY_HR * WAGE_PER_HR;
-                        break;
-
-                }
+        if (checkAttendance == Constants.IS_ABSENT) {
+            System.out.println("Employee is Absent.");
+        } else {
+            System.out.println("Employee is Present.");
+            int empType = random.nextInt() % 2;
+            if (empType < 0) {
+                empType = -empType;
             }
+            switch (empType) {
+                case Constants.IS_PART_TIME:
+                        System.out.println("Employee is Part Time.");
+                        total_wage = Constants.WAGE_PER_HR * Constants.PART_TIME_HR;
+                        total_hrs = total_hrs + Constants.PART_TIME_HR;
+                    break;
+                case Constants.IS_FULL_TIME:
+                        System.out.println("Employee is Full Time.");
+                        total_wage = Constants.WAGE_PER_HR * Constants.FULL_DAY_HR;
+                        total_hrs = total_hrs + Constants.FULL_DAY_HR;
+                        break;
+            }
+        }
             System.out.println("Total Wage = " + total_wage);
+            System.out.println("Total Hours = " + total_hrs);
         }
     }
+
+
 }
